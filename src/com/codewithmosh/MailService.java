19dc35841fa903bde6f31ca10b782805a5a77f40
dmurooka <dmurooka@gmail.com>
2023-05-18 17:41:15 +0900

    Learned stack

---
+++ src/com/codewithmosh/MailService.java
@@ -0,0 +1,23 @@
+package com.codewithmosh;
+
+public class MailService {
+    public void sendEmail() {
+        connect();
+        authenticate();
+        // Send email
+        disconnect();
+    }
+
+    private void connect() {
+        System.out.println("Connect");
+
+    }
+
+    private void disconnect() {
+        System.out.println("Disconnect");
+    }
+
+    private void authenticate() {
+        System.out.println("Authenticate");
+    }
+}
---
+++ src/com/codewithmosh/Main.java
@@ -0,0 +1,13 @@
+package com.codewithmosh;
+
+public class Main {
+    public  static void main(String[] arg) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack.peek());
+
+    }
+}
